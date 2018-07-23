7cfc36069984562f4a5729721afb56ec73160fdb
furikuda <19654092+Furikuda@users.noreply.github.com>
2018-07-23 14:51:37 +0200

    init for web

---
+++ index.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+Encoding.default_external = Encoding::UTF_8
+
+require "pp" # don't remove, this adds the pretty_inspect method
+require "securerandom"
+require "sequel"
+require "sinatra"
+require "slim"
+
+require_relative "./site.rb"
+
+$DATABASE = Sequel.sqlite("grepsocial.sqlite")
+
+set :bind, '0.0.0.0'
+
+set :public_folder, File.join(File.dirname(__FILE__), "public")
+set :show_exceptions, false
+
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+      username == 'admin' and password == 'pwd'
+end
+
+use Rack::Session::Cookie,
+    :key => 'rack.session',
+    :path => '/',
+    :expire_after => 2592000, # In seconds
+    :secret => SecureRandom.hex
+
+def mark_seen(item)
+    $DATABASE[:grepsocial].where(site:item[:site], identifier:item[:identifier]).update(seen: true)
+end
+
+def get_unseen_items(nb:10)
+    rows = $DATABASE[:grepsocial].where(seen:false).limit(nb).map{|row| row.to_h}.each{|row| row['debug'] = row.pretty_inspect }
+    return rows
+end
+
+set :items_cache, {}
+
+before do
+    Dir.glob(File.join("sites-enabled", "*.rb")).each do |site|
+        load "#{site}"
+    end
+end
+
+get "/unseen" do
+    nb = 10
+    if params[:nb]=~/^(\d+)$/
+        if nb < 1000
+            nb = $1.to_i
+        end
+    end
+
+    results = get_unseen_items(nb:nb)
+    randid = rand(10000000000)
+    settings.items_cache[randid] = results
+    session["randid"] = randid
+    content_type :json
+    return results.to_json
+end
+
+post "/markseen" do
+    if session
+        seen = settings.items_cache[session[:randid]] || []
+        seen.each do |item|
+            mark_seen(item)
+        end
+    end
+    redirect "https://#{request.host}/"
+
+end
+
+get '/' do
+    slim :main
+end
---
+++ public/js/gs.js
@@ -0,0 +1,97 @@
+function getUnseen(nb=100){
+    $.ajax({
+        url: "/unseen?nb="+nb,
+        type: "GET",
+        dataType: "json",
+        success: function(data, textStatus) {
+            showPics(data,true);
+        },
+        error: function (data) {
+//            toastr.options.timeOut = 2500;
+//            toastr.options.positionClass= "toast-top-center";
+//            toastr.error(data.responseText);
+			console.log("Error: "+data.responseText); 
+        }
+    });
+}
+
+function escapeXml(txt) {
+    return txt.replace(/[<>&'"]/g, function (c) {
+        switch (c) {
+            case '<': return '&lt;';
+                case '>': return '&gt;';
+                case '&': return '&amp;';
+                case '\'': return '&apos;';
+                case '"': return '&quot;';
+            }
+    });
+}
+
+function urlToSVGPic(url) {
+    var text_tag = "<text x='0' y='15' fill='blue' font-size='16'>";
+
+    var uri = new URL(url);
+    var txt_escaped = escapeXml(uri.hostname + uri.pathname);
+    var bits = txt_escaped.match(/.{1,30}/g)
+    for (i=0; i<bits.length; i++) {
+      text_tag += "<tspan x='0' dy='1.2em'>"+bits[i]+"</tspan>";
+    }
+
+    text_tag += "</text>";
+    var svg = "<svg xmlns='http://www.w3.org/2000/svg' version='1.1'>"+text_tag+"</svg>"
+    var img_data = "data:image/svg+xml;utf8,"+svg;
+    return img_data;
+}
+
+function showPics(data,save=true) {
+    console.log("save "+save);
+	if (typeof data === 'undefined') {
+		return;
+	}
+    if (data.length == 0) {
+        var container = $('#container');
+        $('<div/>')
+            .appendTo(container)
+            .text("That's all folks!") 
+        return;
+    }
+    var itemList = $('#container');
+   	$.each(data, function(i) {
+        var item = data[i];
+		var li = $('<div/>')
+        .addClass('thumb')
+        .addClass('item')
+        .addClass(item['site']+'-item');
+        if (window.location.hash == '#debug') {
+            li.attr('data-tooltip', item['debug'])
+        }
+        li.appendTo(itemList);
+
+		var aaa = $('<a/>')
+        .attr('href',item['source'])
+        .attr('target','#blank');
+        if (item['thumb'] == "") {
+            aaa.css({'background-image': 'url("'+ urlToSVGPic(item['url']) +'")'})
+        } else  {
+            aaa.css({'background-image': 'url("'+item['thumb']+'")'})
+        }
+        aaa.appendTo(li);
+        var overlay = $('<img/>')
+        .attr('src',"/pics/"+item['site']+".png")
+        .addClass('overlay')
+        .appendTo(li);
+        if (save) {
+            var c_label  = $('<label/> ')
+                .addClass("checkbox-label")
+                .html("&nbsp;&nbsp;&nbsp;Save")
+                .appendTo(li);
+            var c_id=[item['source'],item['id']].join("-@@|@@-");
+            var checkbox = $('<input type="checkbox" id="'+c_id+'"/>')
+                .addClass("save-checkbox")
+                .change(function() {
+                    $.post("/save", {id: this.id, checked: this.checked});
+                })
+                .prependTo(c_label);
+        }
+    });
+}
