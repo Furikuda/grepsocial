0638db6a3ae08ceff6857e014ed5dd2401bbc88e
furikuda <19654092+Furikuda@users.noreply.github.com>
2022-10-31 11:32:03 +0100

    fix twitter

--- sites-available/twitter.rb
+++ sites-available/twitter.rb
@@ -3,6 +3,9 @@ require_relative "../site.rb"
 class TwitterSite < SocialSite
   require "twitter"
 
+  class TwitterError < Exception
+  end
+
   attr_accessor :twitter
   def initialize()
     super()
@@ -38,7 +41,7 @@ class TwitterSite < SocialSite
     end
   end
 
-  def fetch_new(max_results: 450)
+  def fetch_new(max_results: 200)
       # Because we need to be careful of rate limiting
       new_items = []
       max_per_tag = max_results/( @tags.size + 1 )
@@ -49,57 +52,71 @@ class TwitterSite < SocialSite
       return new_items
   end
 
+  def extract_si(tweet)
+    sis = []
+    if tweet.quoted_status?
+      quoted_tweet = @twitter.statuses(tweet.quoted_tweet.id, tweet_mode:'extended')[0]
+      return extract_si(quoted_tweet)
+    end
+    if tweet.media?
+      tweet.media.each do |m|
+        si = SocialSite::Item.new()
+        si.site = "Twitter"
+        si.date = Time.now.to_i
+        si.url = tweet.url.to_s
+        si.source = tweet.url.to_s
+        si.type = m.type
+        si.thumb = m.media_uri.to_s
+        if m.type == "photo"
+          si.identifier = m.media_uri.to_s
+        elsif m.type == "video" or m.type == "animated_gif"
+          vi = m.video_info
+          video_url = vi.attrs[:variants].select{|var| var[:content_type].start_with?('video/')}.sort_by{|var| var[:bitrate]}[0][:url]
+          si.identifier = video_url
+        else
+          raise Exception.new("unsupported type in #{tweet.url} : #{tweet.type}")
+        end
+        sis << si
+      end
+    end
+    if tweet.urls?
+      tweet.urls.each do |turl|
+        si = SocialSite::Item.new()
+        si.site = "Twitter"
+        si.date = Time.now.to_i
+        si.url = tweet.url.to_s
+        si.source = tweet.url.to_s
+        si.type = "link"
+        si.identifier = turl.expanded_url.to_s
+        sis << si
+      end
+    end
+    return sis
+  end
+
   def fetch_new_tag(tag, max_results:100)
     items = []
     connect if not @twitter
-    results = @twitter.search(tag, result_type: "recent", count:max_results, tweet_mode:'extended')
+    begin
+      results = @twitter.search(tag, result_type: "recent", count:max_results, tweet_mode:'extended')
+    rescue Twitter::Error::TooManyRequests
+      return []
+    end
     results.each do |t|
       next if t.retweet?
-      if t.media?
-        t.media.each do |m|
-          si = SocialSite::Item.new()
-          si.site = "Twitter"
-          si.date = Time.now.to_i
-          si.url = t.url.to_s
-          si.source = t.url.to_s
-          si.type = m.type
-          if m.type == "photo"
-            si.identifier = m.media_uri.to_s
-            si.thumb = m.media_uri.to_s
-          elsif m.type == "video"
-            si.thumb = m.media_uri.to_s
-            vi = m.video_info
-            video_url = vi.attrs[:variants].select{|var| var[:content_type].start_with?('video/')}.sort_by{|var| var[:bitrate]}[0][:url]
-            si.identifier = video_url
-          else
-            raise Exception.new("unsupported type in #{t.url}")
-          end
-          items << si
-        end
-      end
-      if t.urls?
-        t.urls.each do |turl|
-          si = SocialSite::Item.new()
-          si.site = "Twitter"
-          si.date = Time.now.to_i
-          si.url = t.url.to_s
-          si.source = t.url.to_s
-          si.type = "link"
-          si.identifier = turl.expanded_url.to_s
-          items << si
-        end
-      end
+      items.concat(extract_si(t))
     end
     return items
   end
 end
 
-#t = TwitterSite.new()
-#t.connect
-#
-#res = t.fetch_new_tag('glc2021')
-#puts res.size
-#res.each do |s|
-#  pp s
-#end
+if __FILE__ == $0
+  t = TwitterSite.new()
+  t.connect
+ # tweet = t.twitter.statuses("1458903961054896134")[0]
+  pp t.fetch_new_tag("test")
+#  pp tweet.to_h
+#  binding.pry
+  pp t.extract_si(tweet)
+end
 
